bf0526f70a8bc4898c754da4ca26aa052bfbeacc
Alison Martinez <alison.a.martinez@outlook.com>
2023-03-08 13:00:26 -0500

    bug fixes

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Client from "../lib/Client";
+import Config from "../lib/Config";
+import { TestLabContext, TestLabProvider } from "../lib/ContextProvider";
+
+export { Client, Config, TestLabContext, TestLabProvider };
